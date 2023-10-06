89d738e7e22c548a56690317a99267ee5a9636a2
thotam <thanhtamtqno1@gmail.com>
2023-10-06 08:01:51 +0700

    Update GoogleDriveAdapter.php

--- src/GoogleDriveAdapter.php
+++ src/GoogleDriveAdapter.php
@@ -339,7 +339,6 @@ class GoogleDriveAdapter implements FilesystemAdapter
     {
         $client = $this->service->getClient();
         if ($client->isAccessTokenExpired()) {
-            $client->getCache()->clear();
             if ($client->isUsingApplicationDefaultCredentials()) {
                 $client->fetchAccessTokenWithAssertion();
             } else {
